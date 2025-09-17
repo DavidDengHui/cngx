c1f5910cfb30bfce5ed4d1c63657fbba2cffc828
DavidDengHui <david.no.1@msn.cn>
2025-09-18 05:07:07 +0800

    修复移动端页脚链接

--- footer.php
+++ footer.php
@@ -10,16 +10,16 @@ if (basename($_SERVER['PHP_SELF']) == 'footer.php') {
     <footer class="footer">
         <div class="footer-content">
             <div class="footer-left">
-                csngx.cn © 2025 版权所有
+                <a href="/">csngx.cn</a> © 2025<?php if(date('Y')>=2025) echo ' - '.date('Y'); ?> 版权所有
             </div>
             <div class="footer-right">
-                湘ICP备17019987号 | 湘公网安备43020002000058号
+                <img style="display: inline; width: 1rem; object-fit: contain; vertical-align: top; opacity: 1; margin: 0 5px;" src="/files/icpba.png" class="lazyload placeholder entered loaded" data-srcset="/files/icpba.png" srcset="/files/icpba.png" data-ll-status="loaded"><a href="https://beian.miit.gov.cn/" target="_blank">湘ICP备17019987号</a> | <img style="display: inline; width: 1rem; object-fit: contain; vertical-align: top; opacity: 1; margin: 0 5px;" src="/files/gaba.png" class="lazyload placeholder entered loaded" data-srcset="/files/gaba.png" srcset="/files/gaba.png" data-ll-status="loaded"><a href="https://beian.mps.gov.cn/#/query/webSearch?code=43020002000058" target="_blank">湘公网安备43020002000058号</a>
             </div>
         </div>
         <div class="footer-mobile">
-            <div>csngx.cn © 2025 版权所有</div>
-            <div>湘ICP备17019987号</div>
-            <div>湘公网安备43020002000058号</div>
+            <div><a href="/">csngx.cn</a> © 2025<?php if(date('Y')>=2025) echo ' - '.date('Y'); ?> 版权所有</div>
+            <div><a href="https://beian.miit.gov.cn/" target="_blank">湘ICP备17019987号</a></div>
+            <div><a href="https://beian.mps.gov.cn/#/query/webSearch?code=43020002000058" target="_blank">湘公网安备43020002000058号</a></div>
         </div>
         
         <!-- 预留网站统计代码区域 -->
@@ -35,6 +35,15 @@ if (basename($_SERVER['PHP_SELF']) == 'footer.php') {
             padding: 20px;
             margin-top: auto;
             font-size: 14px;
+        }
+        
+        .footer a {
+            color: white;
+            text-decoration: none;
+        }
+        
+        .footer a:hover {
+            text-decoration: underline;
         }
         
         .footer-content {
