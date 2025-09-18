f44b569172ec809876e3dab0f7415f7fc8810d7e
DavidDengHui <david.no.1@msn.cn>
2025-09-18 17:28:31 +0800

    修复footer隐蔽式页脚

--- footer.php
+++ footer.php
@@ -10,16 +10,16 @@ if (basename($_SERVER['PHP_SELF']) == 'footer.php') {
     <footer class="footer">
         <div class="footer-content">
             <div class="footer-left">
-                <a href="/">csngx.cn</a> © 2025<?php if(date('Y')>=2025) echo ' - '.date('Y'); ?> 版权所有
+                <img src="/files/logo.svg"><a href="/">csngx.cn</a> © 2025<?php if(date('Y')>2025) echo ' - '.date('Y'); ?> 版权所有
             </div>
             <div class="footer-right">
-                <img style="display: inline; width: 1rem; object-fit: contain; vertical-align: top; opacity: 1; margin: 0 5px;" src="/files/icpba.png" class="lazyload placeholder entered loaded" data-srcset="/files/icpba.png" srcset="/files/icpba.png" data-ll-status="loaded"><a href="https://beian.miit.gov.cn/" target="_blank">湘ICP备17019987号</a> | <img style="display: inline; width: 1rem; object-fit: contain; vertical-align: top; opacity: 1; margin: 0 5px;" src="/files/gaba.png" class="lazyload placeholder entered loaded" data-srcset="/files/gaba.png" srcset="/files/gaba.png" data-ll-status="loaded"><a href="https://beian.mps.gov.cn/#/query/webSearch?code=43020002000058" target="_blank">湘公网安备43020002000058号</a>
+                <img src="/files/gaba.ico"><a href="https://beian.mps.gov.cn/#/query/webSearch?code=43020002000058" target="_blank">湘公网安备43020002000058号</a> | <img src="/files/icpba.ico" ><a href="https://beian.miit.gov.cn/" target="_blank">湘ICP备17019987号</a>
             </div>
         </div>
         <div class="footer-mobile">
-            <div><a href="/">csngx.cn</a> © 2025<?php if(date('Y')>=2025) echo ' - '.date('Y'); ?> 版权所有</div>
-            <div><a href="https://beian.miit.gov.cn/" target="_blank">湘ICP备17019987号</a></div>
-            <div><a href="https://beian.mps.gov.cn/#/query/webSearch?code=43020002000058" target="_blank">湘公网安备43020002000058号</a></div>
+            <div><img src="/files/logo.svg"><a href="/">csngx.cn</a> © 2025<?php if(date('Y')>2025) echo ' - '.date('Y'); ?> 版权所有</div>
+            <div><img src="/files/gaba.ico"><a href="https://beian.mps.gov.cn/#/query/webSearch?code=43020002000058" target="_blank">湘公网安备43020002000058号</a></div>
+            <div><img src="/files/icpba.ico"><a href="https://beian.miit.gov.cn/" target="_blank">湘ICP备17019987号</a></div>
         </div>
         
         <!-- 预留网站统计代码区域 -->
@@ -30,20 +30,28 @@ if (basename($_SERVER['PHP_SELF']) == 'footer.php') {
     
     <style>
         .footer {
-            background-color: #2c3e50;
-            color: white;
+            background-color: #f5f5f5;
+            color: #999;
             padding: 20px;
             margin-top: auto;
-            font-size: 14px;
+            font-size: 12px;
         }
         
         .footer a {
-            color: white;
+            color: #999;
             text-decoration: none;
         }
         
         .footer a:hover {
             text-decoration: underline;
+        }
+        
+        /* 备案号图标样式 */
+        .footer-content img, .footer-mobile img {
+            width: 1rem;
+            height: auto;
+            margin-right: 5px;
+            vertical-align: middle;
         }
         
         .footer-content {
